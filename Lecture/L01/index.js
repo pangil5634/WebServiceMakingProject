2427c06a49d2053bd2323a0435068a09a958d953
pangil5634 <oksk6681@naver.com>
2023-08-10 15:06:58 +0900

    230810 Lecture 01, 02 Complete

---
+++ Lecture/L01/index.js
@@ -0,0 +1,6 @@
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+const element = document.body; // 해당 요소의 ID를 가져옴
+element.style.width = screenWidth + "px";
+element.style.height = screenHeight+ "px";
